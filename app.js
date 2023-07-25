cb949e140b6a6e7497ce5596ac78b46431fccb1e
neysap <porterneysa@gmail.com>
2023-07-25 17:09:30 -0400

    adding handlers

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Middleware
 app.use(express.json());
@@ -18,10 +23,19 @@ app.use(session({
   saveUninitialized: true,
 }));
 
+// Handlebars helper to check if the user is logged in
+app.use((req, res, next) => {
+  res.locals.loggedIn = req.session.logged_in;
+  res.locals.username = req.session.username;
+  next();
+});
+
 // Test the server
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  const posts = []; // Replace this with the logic to fetch posts from the database
+  res.render('homepage', { loggedIn: req.session.logged_in, username: req.session.username, posts });
 });
+
 
 // Start the server
 sequelize.sync({ force: false }).then(() => {
