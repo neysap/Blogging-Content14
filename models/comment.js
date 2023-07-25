68e5be1b8cb414637ba06c93c6156a2caca4989f
neysap <porterneysa@gmail.com>
2023-07-25 16:57:38 -0400

    adding-models

---
+++ models/comment.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment_text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+  
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+  });
+
+module.exports = Comment;
